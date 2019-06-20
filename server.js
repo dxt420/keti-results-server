29336855edd2d2f5466df179949ea72ebef7c216
dxt420 <omonaderrick25@gmail.com>
2019-06-21 01:26:38 +0300

    AAA

--- server.js
+++ server.js
@@ -114,6 +114,9 @@ app.post('/messageConsult', async (req, res) => {
   // console.log(req);
   console.log(req.body);
   console.log(req.body.token);
+  console.log("||||||||||||||  BODY ||||||||||||");
+  console.log(req);
+  
   console.log("||||||||||||||  TOKEN ||||||||||||");
   console.log(req.body['token']);
   const chat = {
